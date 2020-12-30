ea9a2b31c3360b88c4396e6119e4cdc9073c2805
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-12-30 00:59:53 +0000

    wc: does not properly count multibyte characters
    (Solaris patch which has also been merged to ksh master via
     https://github.com/att/ast/issues/122 )

--- src/lib/libcmd/wclib.c
+++ src/lib/libcmd/wclib.c
@@ -18,6 +18,9 @@
 *                  David Korn <dgk@research.att.com>                   *
 *                                                                      *
 ***********************************************************************/
+/*
+ * Copyright (c) 2007, 2012, Oracle and/or its affiliates. All rights reserved.
+ */
 #pragma prototyped
 /*
  * David Korn
@@ -341,6 +344,8 @@ int wc_count(Wc_t *wp, Sfio_t *fd, const char* file)
 		int		xspace;
 		int		wasspace = 1;
 		unsigned char*	start;
+		int             flagm = 0;
+
 
 		lastchar = 0;
 		start = (endbuff = side) + 1;
@@ -367,6 +372,7 @@ int wc_count(Wc_t *wp, Sfio_t *fd, const char* file)
 			if(mbc(lasttype))
 			{
 				c = lasttype;
+				flagm = 1;
 				goto mbyte;
 			}
 			if(!lasttype && spc(type[*cp]))
@@ -415,6 +421,18 @@ int wc_count(Wc_t *wp, Sfio_t *fd, const char* file)
 							skip = (c&7);
 							adjust += skip;
 							state = 0;
+							if (flagm == 1) {
+								flagm = 0;
+								oldc = *cp;
+								if (xspace && (
+								    iswspace
+								    (*cp)
+								    == 1)) {
+									state
+									    = 8;
+								}
+								continue;
+							}
 							if(skip==2 && (cp[-1]&0xc)==0 && (state=(cp[-1]&0x3)))
 								oldc = *cp;
 							else if(xspace && cp[-1]==0xc2)
